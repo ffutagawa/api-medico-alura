853ffc6913d27189d2a6178cb27b7b023071a9fc
FFutagawa <ffutagawa@gmail.com>
2024-06-24 21:23:01 -0300

    Paginacao / sort

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,11 +33,19 @@ public class MedicoController {
 	}
 	
 	@GetMapping
-	public List<DadosListagemMedico> listar(){
-		return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
+		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
 		
 	}
 }
+
+
+
+
+
+
+
+
 
 
 
