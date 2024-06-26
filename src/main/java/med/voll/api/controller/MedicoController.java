77392e6bf8091a72602c7252de969b460525990c
FFutagawa <ffutagawa@gmail.com>
2024-06-26 19:59:18 -0300

    Inserido metodo de atualizar medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,15 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -37,6 +40,14 @@ public class MedicoController {
 		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
 		
 	}
+	
+	@PutMapping
+	@Transactional
+	public void atualizar (@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		var medico = medicoRepository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
+	}
+	
 }
 
 
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -76,6 +77,21 @@ public class Medico {
 
 	public Endereco getEndereco() {
 		return endereco;
+	}
+
+	public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+		if (dados.nome() != null) {
+			this.nome = dados.nome();
+		}
+		if(dados.telefone() != null) {
+			this.telefone = dados.telefone();
+		}
+		
+		if(dados.endereco() != null) {
+			this.endereco.atualizarinformacoes(dados.endereco());
+		}
+		
+		
 	}	
 
 }
