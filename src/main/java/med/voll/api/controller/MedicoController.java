f8bee73d9233a94290553a703b7d0a1c1ad7395c
FFutagawa <ffutagawa@gmail.com>
2024-06-27 22:04:19 -0300

    Padronizando retornos da API e fazendo melhorias nos metodos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosDetalhamentoMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -27,22 +30,35 @@ import med.voll.api.medico.MedicoRepository;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-	
+
 	@Autowired
 	private MedicoRepository medicoRepository;
 
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-	medicoRepository.save(new Medico(dados));
+	public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+		var medico = new Medico(dados);
+		medicoRepository.save(medico);
+
+		var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+
 	}
-	
+
 	@GetMapping
-	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
-		return medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
-		
+	public ResponseEntity<Page<DadosListagemMedico>> listar(
+			@PageableDefault(size = 10, page = 0, sort = { "nome" }) Pageable paginacao) {
+		var page = medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+		return ResponseEntity.ok(page);
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity BuscarPorId(@PathVariable Long id) {
+		var medico = medicoRepository.getReferenceById(id);
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+	}
+
 //	@GetMapping
 //	public List<Medico> findAll(){
 //		return medicoRepository.findAll();
@@ -50,37 +66,33 @@ public class MedicoController {
 
 	@PutMapping
 	@Transactional
-	public void atualizar (@RequestBody @Valid DadosAtualizacaoMedico dados) {
-		var medico = medicoRepository.getReferenceById(dados.id());
-		medico.atualizarInformacoes(dados);
-	}
-	
+	public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+	        var medico = medicoRepository.getReferenceById(dados.id());
+	        medico.atualizarInformacoes(dados);
+
+	        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+	    }
+
 //	@DeleteMapping("/{id}")
 //	@Transactional
 //	public void deletar (@PathVariable Long id) {
 //		medicoRepository.deleteById(id);
 //	}
-	
+
+//	@DeleteMapping("/{id}")
+//	@Transactional
+//	public void deletar (@PathVariable Long id) {
+//		var medico = medicoRepository.getReferenceById(id);
+//		medico.excluir();
+//	}
+
 	@DeleteMapping("/{id}")
 	@Transactional
-	public void deletar (@PathVariable Long id) {
+	public ResponseEntity deletar(@PathVariable Long id) {
 		var medico = medicoRepository.getReferenceById(id);
 		medico.excluir();
+
+		return ResponseEntity.noContent().build();
 	}
-	
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -4,12 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosAtualizacaoMedico(
-		
-		@NotNull
-		Long id,
-		String nome,
-		String telefone,
-		DadosEndereco endereco) {
-
-	
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
 }
