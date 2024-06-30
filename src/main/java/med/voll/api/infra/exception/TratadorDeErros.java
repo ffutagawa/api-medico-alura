cb5d745df129c4fc88687b633716be9996f5c111
FFutagawa <ffutagawa@gmail.com>
2024-06-30 19:58:59 -0300

    Implementando Spring Security para Login e senha

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,6 +1,5 @@
-package med.voll.api.infra;
+package med.voll.api.infra.exception;
 
-import org.hibernate.type.descriptor.jdbc.VarbinaryJdbcType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
